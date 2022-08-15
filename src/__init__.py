a400c30feb8e5db99c32e8e7fa7a9fa8bc158abf
mentix02 <manan.yadav02@gmail.com>
2022-08-15 13:21:13 +0530

    Changed meta variable ordering

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
+__version__ = "0.0.2"
+__author__ = "mentix02"
+
 from .runner import run
 from .api import get_orders, Order, FoodItem
-
-__author__ = "mentix02"
-__version__ = "0.0.1a"
 
 __all__ = ("run", "get_orders", "Order", "FoodItem")
