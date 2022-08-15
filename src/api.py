39857ee43d918077a59e2828ca3d221af2e38004
mentix02 <manan.yadav02@gmail.com>
2022-08-15 12:38:07 +0530

    Made Order & FoodItem inherit Serializable and finished get_orders method

--- src/api.py
+++ src/api.py
@@ -1,20 +1,32 @@
+import sys
+import json
 import requests
 import dataclasses
 
 from .payload import Payload
+from .utils import Serializable
 from .configurator import Config
+from .constants import DEFAULT_URL
 
 
 @dataclasses.dataclass
-class FoodItem:
+class FoodItem(Serializable):
 
     name: str
     cost: float
     quantity: int
 
+    def to_json(self) -> str:
+        return json.dumps(self.to_dict(), indent=2)
+
+    def to_dict(self) -> dict:
+        return dataclasses.asdict(self) | {"total_cost": self.total_cost}
+
     @classmethod
     def from_dict(cls, data: dict) -> "FoodItem":
-        return cls(name=data["name"], cost=data["unitCost"], quantity=data["quantity"])
+        return FoodItem(
+            name=data["name"], cost=data["unitCost"], quantity=data["quantity"]
+        )
 
     @property
     def total_cost(self) -> float:
@@ -22,20 +34,33 @@ class FoodItem:
 
 
 @dataclasses.dataclass
-class Order:
+class Order(Serializable):
 
     id: int
     date: str
     customer: str
     items: list[FoodItem] = dataclasses.field(default_factory=list)
 
+    def to_json(self) -> str:
+        return json.dumps(self.to_dict())
+
+    def to_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "date": self.date,
+            "customer": self.customer,
+            "items": [item.to_dict() for item in self.items],
+        }
+
     @classmethod
     def from_dict(cls, data: dict) -> "Order":
 
         date = data["createdAt"].split("T")[0]
-        items = [FoodItem.from_dict(item) for item in data["items"]["dishes"]]
+        items = [
+            FoodItem.from_dict(item) for item in data["cartDetails"]["items"]["dishes"]
+        ]
 
-        return cls(
+        return Order(
             date=date,
             items=items,
             id=data["id"],
@@ -46,11 +71,34 @@ class Order:
         return len(self.items)
 
 
-def get_orders(url: str, payload: Payload, config: Config) -> list[Order]:
-    req = requests.post(
-        url,
-        data=payload,
-        headers=config.headers,
-        cookies=config.cookies,
-    )
-    return [Order.from_dict(order) for order in req.json()["orders"]]
+def get_orders(payload: Payload, config: Config, url: str = DEFAULT_URL) -> list[Order]:
+
+    orders: list[Order] = []
+
+    with requests.Session() as session:
+
+        session.headers.update(config.headers)
+        session.cookies.update(config.cookies)
+
+        while True:
+
+            response = session.post(url, data=payload.to_json())
+
+            if not response.ok:
+                sys.stderr.write(f"error: {response.status_code} {response.reason}")
+                continue
+
+            resp_data = response.json()
+
+            if new_orders := resp_data.get("orders"):
+                orders.extend([Order.from_dict(order) for order in new_orders])
+            else:
+                print(resp_data)
+
+            if len(new_orders) < payload.count:
+                # No more orders to fetch
+                break
+
+            payload.offSet += payload.count
+
+    return orders
