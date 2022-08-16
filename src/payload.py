fbc70c7a96808295cafdb56634b3624345584ea3
mentix02 <manan.yadav02@gmail.com>
2022-08-16 23:37:30 +0530

    Added days member to Payload

--- src/payload.py
+++ src/payload.py
@@ -13,20 +13,25 @@ class Payload(Serializable):
     """Payload structure to used to query the Zomato API"""
 
     res_ids: list[int]
+    days: int = DEFAULT_DAYS
     end_date: Optional[str] = None
     count: int = 20
     offSet: int = 0
     state: str = "DELIVERED"
 
     def __post_init__(self):
+
         if self.end_date is None:
             self.end_date = datetime.datetime.now().strftime(DATE_FORMAT)
+
+        if self.days > 10 or self.days < 1:
+            raise ValueError("days cannot be less than 0 or greater than 10")
 
     @property
     def start_date(self) -> str:
         return (
             datetime.datetime.strptime(self.end_date, DATE_FORMAT)
-            - datetime.timedelta(days=10)
+            - datetime.timedelta(days=self.days)
         ).strftime(DATE_FORMAT)
 
     def to_json(self) -> str:
