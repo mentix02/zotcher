8dc9d39fe6a75973858fe79f62b2e7bcb879ca99
mentix02 <manan.yadav02@gmail.com>
2022-08-17 21:39:57 +0530

    Finished version 0.0.2

--- zotcher.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-from src import run
-
-if __name__ == "__main__":
-    run()
--- zotcher/__init__.py
+++ zotcher/__init__.py
@@ -1,7 +1,6 @@
 __version__ = "0.0.2"
 __author__ = "mentix02"
 
-from .runner import run
 from .api import get_orders, Order, FoodItem
 
-__all__ = ("run", "get_orders", "Order", "FoodItem")
+__all__ = ("get_orders", "Order", "FoodItem")
